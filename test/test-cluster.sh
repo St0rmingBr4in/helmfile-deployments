121f31985b0175f0c7a51cff60823d2fff4dd0ca
Julien DOCHE <julien.doche@gmail.com>
2020-02-21 02:18:23 +0100

    CI: Add travis.yml and test scripts
    Signed-off-by: Julien DOCHE <julien.doche@gmail.com>

---
+++ test/test-cluster.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+set -x -e
+
+# Get helmfile
+curl -fsSL -o /tmp/helmfile https://github.com/roboll/helmfile/releases/download/v0.100.1/helmfile_linux_amd64
+
+sudo install /tmp/helmfile /usr/local/bin/
+
+# Get helm
+sudo snap install helm --classic
+
+# Get helm diff plugin
+helm plugin install https://github.com/databus23/helm-diff --version master
+
+# Get kubectl
+sudo snap install kubectl --classic
+
+# shellcheck source=env
+. ./test/env
+
+kubectl apply -R -f raw_k8s
+
+set +e
+
+helmfile apply
